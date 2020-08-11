d5b5a970769e010526548577dc6bce2d424bea41
chd000 <stibbot@yandex.ru>
2020-08-11 12:24:33 +0300

    deleted page with questions list

--- static/js/check.js
+++ static/js/check.js
@@ -1,29 +1,37 @@
-function checkedId() {
-    let checkboxes = document.getElementsByClassName('checkbox');
-    let checkedId = []
-    for (let k = 0; k < checkboxes.length; k++) {
-        if (checkedId.indexOf(checkboxes[k].id) === -1) {
-            checkedId.push(checkboxes[k].id)
-        }
-    }
-    return checkedId
+(function ($) {
+    $(document).on("click", "#nextButton", function () {
+        $.ajax({
+            type: "GET",
+            url: 'http://127.0.0.1:8000/result',
+            data: {
+                list: getCheckedCheckBoxes()
+            },
+            dataType: 'json',
+            success: function () {
+            }
+        })
+        alert('Ваши ответы отправлены, нажмите "Завершить тест"')
+        location.href = 'http://127.0.0.1:8000/result'
+    })
+})(jQuery);
+
+document.getElementById('myselect').addEventListener('change', function () {
+    let getValue = this.value;
+    console.log(getValue);
+});
+
+function getValue() {
+    let select = document.getElementById('myselect');
+    let value = select.value;
+    console.log(value);
 }
 
 function getCheckedCheckBoxes() {
     let checkboxes = document.getElementsByClassName('checkbox');
     let checkboxesChecked = [];
-
-    for (let i = 0; i < 20; i++) {
-        let checkboxesList = []
-        checkboxesChecked.push(checkboxesList)
-    }
     for (let index = 0; index < checkboxes.length; index++) {
         if (checkboxes[index].checked) {
-            for (let j = 0; j < checkedId().length; j++) {
-                if (checkboxes[index].id === checkedId()[j]) {
-                    checkboxesChecked[j].splice(j, 0, checkboxes[index].value)
-                }
-            }
+            checkboxesChecked.push(checkboxes[index].value);
         }
     }
     console.log(checkboxesChecked)
