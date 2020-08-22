aad64145a8d91f900efb16ed8148c125768b2221
chd000 <stibbot@yandex.ru>
2020-08-22 15:07:31 +0300

    pk field

--- testapp/testing/question_chooser.py
+++ testapp/testing/question_chooser.py
@@ -10,12 +10,12 @@ def create_all_question_list():
         answers = elements['fields']['answers'].split(sep=';')
         right_answers = elements['fields']['right_answer'].split(sep=';')
         picture = elements['fields']['picture']
-        #q_id = elements['fields']['id']
-        all_questions_lst.append((question_type, question, answers, right_answers, picture, ))
+        q_id = elements['pk']
+        all_questions_lst.append((question_type, question, answers, right_answers, picture, q_id))
 
 
 questions = Questions.objects.all()
-edited_questions = json.loads(serializers.serialize('json', questions, fields=('quest', 'answers', 'right_answer', 'quest_type', 'picture',)))
+edited_questions = json.loads(serializers.serialize('json', questions, fields=('quest', 'answers', 'right_answer', 'quest_type', 'picture', )))
 all_questions_lst = list()
 
 create_all_question_list()
