b93065bffed2587dae7343e6c9220632b3fc6d6c
chd000 <stibbot@yandex.ru>
2020-08-27 12:08:56 +0300

    static root

--- testing/settings.py
+++ testing/settings.py
@@ -113,9 +113,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
